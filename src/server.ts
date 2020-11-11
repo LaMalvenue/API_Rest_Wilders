6e12eacad6a30bdba7f5fc0b7213798aae46565f
Anaïs Rouvière <a.rouviere@hotmail.com>
2020-11-11 20:22:30 +0100

    Correction de la gestion des erreurs

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 // *************** Imports ***************
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import asyncHandler from "express-async-handler";
 import mongoose from "mongoose";
 import cors from "cors";
@@ -16,7 +16,7 @@ interface MongoError extends Error {
   code: number;
 }
 function isMongoError(error: Error): error is MongoError {
-  return "code" in error;
+  return error.name === "MongoError";
 }
 // *************** Database ***************
 mongoose
@@ -26,7 +26,9 @@ mongoose
     useCreateIndex: true,
     autoIndex: true,
   })
+  // eslint-disable-next-line no-console
   .then(() => console.log("Connected to database"))
+  // eslint-disable-next-line no-console
   .catch((err) => console.log(err));
 
 // *************** Middleware ***************
@@ -35,7 +37,7 @@ app.use(express.json());
 app.use(cors());
 
 // *************** Routes ***************
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
 });
 
@@ -44,21 +46,21 @@ app.get(versionApi, asyncHandler(WilderController.read));
 app.put(versionApi, asyncHandler(WilderController.update));
 app.delete(versionApi, asyncHandler(WilderController.delete));
 
-app.get("*", (req, res) => {
+app.get("*", (req: Request, res: Response) => {
   res.status(404);
   res.send({ success: false, message: "Wrong address" });
 });
 
-app.use((error: MongoError, req: Request, res: Response) => {
-  if (!isMongoError(error)) {
-    return;
-  }
-  if (error.code === 11000) {
-    res.status(400);
-    res.json({ success: false, message: "The name is already used" });
-  } else {
-    res.status(400);
-    res.json({ success: false, message: "Database error" });
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: Error, req: Request, res: Response, _next: NextFunction) => {
+  if (isMongoError(error)) {
+    if (error.code === 11000) {
+      res.status(400);
+      res.json({ success: false, message: "The name is already used" });
+    } else {
+      res.status(400);
+      res.json({ success: false, message: "Database error" });
+    }
   }
 });
 
